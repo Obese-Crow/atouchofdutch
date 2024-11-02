cf67dba9fd44e30f95dd34c0bfe26b58c1891d8c
Bradley Thornton <bradleymt16@gmail.com>
2024-11-02 09:26:02 -0500

    changed name to app.py

---
+++ api/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+#import requests
+#import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+@app.route('/about')
+def about():
+    return 'About'
