86f73b6f3cf3c269d1a904c895c1fa7c1fed27b0
Cole Bircher <cole.bircher.iv@gmail.com>
2024-11-02 18:29:24 -0500

    added redirection

--- api/app.py
+++ api/app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 #import requests
 #import json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods = ["POST","GET"])
 def home():
+    if request.method == "POST":
+        return redirect(url_for("bidding"))
     return render_template("index.html")
 @app.route('/bidding')
 def bidding():
