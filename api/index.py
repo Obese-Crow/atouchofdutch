94a230ca33624d8f248edeca3b616f53584c995b
Cole Bircher <cole.bircher.iv@gmail.com>
2024-11-02 09:29:28 -0500

    add javascript

--- api/index.py
+++ api/index.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("index.html")
+
 @app.route('/about')
 def about():
     return 'About'
+
+app.run(host="0.0.0.0", port=80)
