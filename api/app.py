c9c0d32f4446b23eabee0be19e692957bc23cc7c
Cole Bircher <cole.bircher.iv@gmail.com>
2024-11-02 23:58:12 -0500

    we failed :(

--- api/app.py
+++ api/app.py
@@ -1,16 +1,37 @@
-from flask import Flask, render_template, request, redirect, url_for
-#import requests
-#import json
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_socketio import join_room, SocketIO
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "hjhjsdahhds"
 
-@app.route('/',methods = ["POST","GET"])
+CORS(app)
+socketio = SocketIO(app)
+
+@app.route('/', methods=["POST", "GET"])
 def home():
     if request.method == "POST":
+        name = request.form.get("name")
+        session["name"] = name
         return redirect(url_for("bidding"))
     return render_template("index.html")
-@app.route('/bidding')
+
+@app.route('/bidding', methods=["POST", "GET"])
 def bidding():
-    return render_template("bidding.html")
-#test
-##
+    if request.method == "POST":
+        winner_name = session.get("name")
+        socketio.emit("winner", {"name": winner_name}, room="restaurant")  # Emit winner
+        return redirect(url_for("winner", name=winner_name))
+    name = session.get("name")
+    return render_template("bidding.html", name=name)
+
+@app.route('/winner/<name>')
+def winner(name):
+    return " " + name + " Wins!"
+
+@socketio.on("connect")
+def connect(auth):
+    join_room("restaurant")
+
+if __name__ == "__main__":
+    socketio.run(app)
